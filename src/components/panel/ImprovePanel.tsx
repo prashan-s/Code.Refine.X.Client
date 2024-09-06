79b35bee49fd97bcbea0fbe54a7747e17d541885
Prashan Samarathunge <prashansamarathunge@gmail.com>
2024-09-07 03:55:12 +0530

    Minor Improvements

--- src/components/panel/ImprovePanel.tsx
+++ src/components/panel/ImprovePanel.tsx
@@ -9,10 +9,41 @@ import {
     ProgressBar,
     ProgressIndicator,
     ProgressLabel,
-    InsightItem,
-    ProgressCircle
+    InsightItem
 } from '@styles/Improve.Panel';
+import CircularProgress, {
+    CircularProgressProps,
+} from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
+function CircularProgressWithLabel(
+    props: CircularProgressProps & { value: number },
+) {
+    return (
+        <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+            <CircularProgress variant="determinate" {...props} />
+            <Box
+                sx={{
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    position: 'absolute',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Typography
+                    variant="caption"
+                    component="div"
+                    sx={{ color: 'text.secondary' }}
+                >{`${Math.round(props.value)}%`}</Typography>
+            </Box>
+        </Box>
+    );
+}
 
 const ImprovePanel: React.FC = () => {
     return (
@@ -32,15 +63,15 @@ const ImprovePanel: React.FC = () => {
             <SectionTitle>Insights</SectionTitle>
             <InsightItem>
                 <span>Loops</span>
-                <ProgressCircle>99%</ProgressCircle>
+                <CircularProgressWithLabel variant="determinate" value={44} />
             </InsightItem>
             <InsightItem>
                 <span>Conditions</span>
-                <ProgressCircle>99%</ProgressCircle>
+                <CircularProgressWithLabel variant="determinate" value={23} />
             </InsightItem>
             <InsightItem>
                 <span>Nested Loops</span>
-                <ProgressCircle>99%</ProgressCircle>
+                <CircularProgressWithLabel variant="determinate" value={100} />
             </InsightItem>
 
             {/* Divider */}
