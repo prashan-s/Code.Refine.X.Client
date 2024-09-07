0c7b7a6c5ce4a4f26c98f8bc59f09c3feb1e7f05
DulaBoy <master@dulanga.com>
2024-09-07 10:05:01 +0530

    Add get snippets

--- src/components/panel/ReusePanel.tsx
+++ src/components/panel/ReusePanel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     PanelContainer,
     SectionTitle,
@@ -6,15 +6,49 @@ import {
 } from '@styles/Common.Panel';
 
 import ReuseCard from '@components/snippets/ReuseCard';
+
 const ReusePanel: React.FC = () => {
+    const [snippets, setSnippets] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://codeanalyser.dulanga.com/api/Snippets');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setSnippets(data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
     return (
         <PanelContainer>
-            {/* Snippets Section */}
             <SectionTitle>Snippets</SectionTitle>
-            {/* Divider */}
-            <ReuseCard id={0} title={'Test'} content={'avx'} language='JAVA' createdDate={'sadad'} ></ReuseCard>
-            <Divider />
-
+            {snippets.map(snippet => (
+                <React.Fragment key={snippet.snippetID}>
+                    <ReuseCard
+                        id={snippet.snippetID}
+                        title={snippet.snippetTitle}
+                        content={snippet.snippetContent}
+                        language={snippet.language}
+                        createdDate={snippet.dateCreated}
+                    />
+                    <Divider />
+                </React.Fragment>
+            ))}
         </PanelContainer>
     );
 };
