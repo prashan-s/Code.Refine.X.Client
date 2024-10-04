d63a64142eee773d0ea9f2cdfeebdc7b003eecdf
DulaBoy <master@dulanga.com>
2024-10-04 19:41:50 +0530

    Revamp visitNode function

--- src/components/codeAnalysis/CategorizeJavaCode.ts
+++ src/components/codeAnalysis/CategorizeJavaCode.ts
@@ -9,12 +9,30 @@ class CategorizeJavaCode {
         const visitNode = (node: any) => {
             if (node.modifiers) {
                 const modifiers = node.modifiers.map((mod: any) => mod.name);
-                if (modifiers.includes('static')) {
-                    blocks.static.push(node.name.identifier);
-                } else if (modifiers.includes('private')) {
-                    blocks.private.push(node.name.identifier);
-                } else if (modifiers.includes('public')) {
-                    blocks.public.push(node.name.identifier);
+
+                if (node.type === 'MethodDeclaration') {
+                    const methodSignature = `${modifiers.join(' ')} ${node.typeTypeOrVoid} ${node.name.identifier}(${node.formalParameters.parameters.map((p: any) => `${p.typeType} ${p.variableDeclaratorId.identifier}`).join(', ')})`;
+                    const methodBody = code.slice(node.methodBody.start.startIndex, node.methodBody.stop.stopIndex + 1);
+                    const methodDeclaration = `${methodSignature} ${methodBody}`;
+
+                    if (modifiers.includes('static')) {
+                        blocks.static.push(methodDeclaration);
+                    } else if (modifiers.includes('private')) {
+                        blocks.private.push(methodDeclaration);
+                    } else if (modifiers.includes('public')) {
+                        blocks.public.push(methodDeclaration);
+                    }
+                }
+
+                if (node.type === 'FieldDeclaration') {
+                    const variableSignature = `${modifiers.join(' ')} ${node.typeType} ${node.variableDeclarators[0].variableDeclaratorId.identifier};`;
+                    if (modifiers.includes('static')) {
+                        blocks.static.push(variableSignature);
+                    } else if (modifiers.includes('private')) {
+                        blocks.private.push(variableSignature);
+                    } else if (modifiers.includes('public')) {
+                        blocks.public.push(variableSignature);
+                    }
                 }
             }
         };
